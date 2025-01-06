003f119c274c975c73f221867f9823174134e4ad
Uttam Pal <stem.uttam@gmail.com>
2025-01-06 23:21:32 +0530

    Update NavBar.jsx

--- qtify/src/components/NavBar/NavBar.jsx
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -6,40 +6,48 @@ import FeedBackModal from "../Modals/FeedBackModal/FeedBackModal";
 import { showToast } from "../../config/helper-methods";
 import { useNavigate } from "react-router-dom";
 
-const Navbar = ({ data, page, songsData }) => {
-  const [isFeedbackClicked, setIsFeedbackClicked] = useState(false);
+const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
+	const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
+	const navigate = useNavigate();
 
-  const handleClick = () => {
-    setIsFeedbackClicked(!isFeedbackClicked);
-  };
+	const _toggleFeedBackModal = (value = false) => {
+		setIsFeedBackModalOpen(value);
+	};
 
-  useEffect(() => {
-    let feedback = document.getElementById("feedback");
-    let body = document.body;
-    if (isFeedbackClicked) {
-      body.style.overflowY = "hidden";
-      feedback?.classList.add("feedbackClicked");
-    } else {
-      body.style.overflowY = "auto";
-      feedback?.classList.remove("feedbackClicked");
-    }
-  }, [isFeedbackClicked]);
+	const _onSuccess = () => {
+		// show toast
+		showToast("Feedback Submitted", "success");
+	};
 
-  return (
-    <>
-      {isFeedbackClicked && (
-        <Feedback onClose={() => setIsFeedbackClicked(false)} />
-      )}
-      <nav className={styles.nav}>
-        <Logo />
-        <Search data={page === "home" ? data : songsData} page={page} />
-        <Button
-          text="GIVE FEEDBACK"
-          eventHandler={{ event: "onClick", handler: handleClick }}
-        />
-      </nav>
-    </>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<nav className={styles.navbar}>
+				<div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
+					{logo ? <Logo id={styles.logo} /> : null}
+				</div>
+				{search ? (
+					<div className={styles.searchWrapper}>
+						<SearchBar
+							placeholder="Search a album of your choice"
+							data={data}
+						/>
+					</div>
+				) : null}
+
+				{feedback ? (
+					<div
+						className={styles.nav_link}
+						onClick={() => _toggleFeedBackModal(true)}>
+						Feedback
+					</div>
+				) : null}
+			</nav>
+			<FeedBackModal
+				isOpen={isFeedBackModalOpen}
+				onSuccess={_onSuccess}
+				onDismiss={_toggleFeedBackModal}
+			/>
+		</div>
+	);
 };
-
 export default NavBar;
