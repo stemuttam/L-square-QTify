a256510b7a6ec3553caa54ed8a3703a49f767892
Uttam Pal <stem.uttam@gmail.com>
2025-01-06 22:53:48 +0530

    Update HeroSection.jsx

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -2,22 +2,16 @@ import React from "react";
 import styles from "./HeroSection.module.css";
 import Headphone from "../../assets/headphone.svg";
 
-const HeroSection = () => {
-	return (
-		<>
-			<div className={styles.wrapper}>
-				<div className={styles.heroImageWrapper}>
-					<div className={styles.banner}>
-						<h1>100 Thousand Songs, ad free</h1>
-						<h1>Over thousands podcast episodes</h1>
-					</div>
-					<div>
-						<img src={Headphone} alt="Headphone" height="212px" width="212px" />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div>
+        <h1> 100 Thousand Songs, ad - free </h1>
+        <h1> Over thousands podcast episodes</h1>
+      </div>
+      <img src={heroImage} alt="Hero_Image" />
+    </div>
+  );
 };
 
 export default HeroSection;
