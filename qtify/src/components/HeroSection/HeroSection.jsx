e99a71c71defe9aa3e2159ce2487ec77a28e99be
Uttam Pal <stem.uttam@gmail.com>
2025-01-07 22:36:01 +0530

    Update HeroSection.jsx

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -8,7 +8,29 @@ const HeroSection = () => {
       className={styles.wrapper}
       data-testid="hero-section" /* For testing the parent element */
     >
-      <div className={styles.heroImageWrapper}>
+      <div className={styles.heroImageWrapper}>import React from "react";
+import styles from "./HeroSection.module.css";
+import Headphone from "../../assets/headphone.svg";
+
+const HeroSection = () => {
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.heroImageWrapper}>
+					<div className={styles.banner}>
+						<h1>100 Thousand Songs, ad free</h1>
+						<h1>Over thousands podcast episodes</h1>
+					</div>
+					<div>
+						<img src={Headphone} alt="Headphone" height="212px" width="212px" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default HeroSection;
         <div className={styles.banner}>
           <h1>100 Thousand Songs, ad-free</h1>
           <h1>Over thousands of podcast episodes</h1>
