a1fff540bc560e84ea0da6006660aa6695c9a723
Uttam Pal <stem.uttam@gmail.com>
2025-01-07 23:47:41 +0530

    Update NavBar.jsx

--- qtify/src/components/NavBar/NavBar.jsx
+++ qtify/src/components/NavBar/NavBar.jsx
@@ -1,54 +1,51 @@
 import React, { useState } from "react";
 import Logo from "../Logo/Logo";
-import styles from "./NavBar.module.css";
+import Button from "../Button/Button";
 import SearchBar from "../SearchBar/SearchBar";
 import FeedBackModal from "../Modals/FeedBackModal/FeedBackModal";
 import { showToast } from "../../config/helper-methods";
 import { useNavigate } from "react-router-dom";
+import styles from "./NavBar.module.css";
 
 const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
-	const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
-	const navigate = useNavigate();
+  const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
+  const navigate = useNavigate();
 
-	const _toggleFeedBackModal = (value = false) => {
-		setIsFeedBackModalOpen(value);
-	};
+  const _toggleFeedBackModal = (value = false) => {
+    setIsFeedBackModalOpen(value);
+  };
 
-	const _onSuccess = () => {
-		// show toast
-		showToast("Feedback Submitted", "success");
-	};
+  const _onSuccess = () => {
+    showToast("Feedback Submitted", "success");
+  };
 
-	return (
-		<div className={styles.wrapper}>
-			<nav className={styles.navbar}>
-				<div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
-					{logo ? <Logo id={styles.logo} /> : null}
-				</div>
-				{search ? (
-					<div className={styles.searchWrapper}>
-						<SearchBar
-							placeholder="Search a album of your choice"
-							data={data}
-						/>
-					</div>
-				) : null}
-
-				{feedback ? (
-					<div
-						className={styles.nav_link}
-						onClick={() => _toggleFeedBackModal(true)}>
-						Give Feedback
-					</div>
-				) : null}
-			</nav>
-			<FeedBackModal
-				isOpen={isFeedBackModalOpen}
-				onSuccess={_onSuccess}
-				onDismiss={_toggleFeedBackModal}
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      <nav className={styles.navbar}>
+        <div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
+          {logo ? <Logo id={styles.logo} /> : null}
+        </div>
+        {search ? (
+          <div className={styles.searchWrapper}>
+            <SearchBar placeholder="Search an album of your choice" data={data} />
+          </div>
+        ) : null}
+        {feedback ? (
+          <div className={styles.nav_link}>
+            <Button
+              text="Give Feedback"
+              onClick={() => _toggleFeedBackModal(true)}
+            />
+          </div>
+        ) : null}
+      </nav>
+      <FeedBackModal
+        isOpen={isFeedBackModalOpen}
+        onSuccess={_onSuccess}
+        onDismiss={_toggleFeedBackModal}
+      />
+    </div>
+  );
 };
 
 export default NavBar;
