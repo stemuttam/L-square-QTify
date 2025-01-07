4d532f7d3d5c0dc4158603f08df49ea26d01988e
Uttam Pal <stem.uttam@gmail.com>
2025-01-08 00:04:53 +0530

    Update Logo.jsx

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logoImage from "../../assets/logo.png"; // Update the path to your actual logo file
-import styles from "./Logo.module.css";
 
 const Logo = ({ id }) => {
   return <img src={logoImage} alt="QTify Logo" className={styles.logo} id={id} />;
